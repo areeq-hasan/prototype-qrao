fix normalization

qrao/multiqubit.py
@@ -29,7 +29,16 @@ measures = [
 ]
 
 operators = [
-    MatrixOp(((np.outer(measure[0], measure[0]) + np.outer(measure[1], measure[1]))))
+    (1 / (np.sqrt(2)))
+    * (
+        (
+            2
+            * MatrixOp(
+                (np.outer(measure[0], measure[0]) + np.outer(measure[1], measure[1]))
+            )
+        )
+        - (I ^ I)
+    )
     for measure in measures
 ]
 
@@ -295,8 +304,12 @@ def generate_operator(
             if coefficient != 0:
                 dvar_i_partition = partitions[dvar_i.partition_idx]
                 dvar_j_partition = partitions[dvar_j.partition_idx]
-                normalization = np.sqrt(
-                    dvar_i_partition.num_dvars * dvar_j_partition.num_dvars
+                normalization = (
+                    dvar_i_partition.num_dvars
+                    ** (1 / (2**dvar_i_partition.num_qubits))
+                ) * (
+                    dvar_j_partition.num_dvars
+                    ** (1 / (2**dvar_j_partition.num_qubits))
                 )
                 dvar_pair_op_term = pad_dvar_operator(
                     dvar_i.operator, dvar_i_partition.qubits, num_qubits

tests/test_multiqubit.py
@@ -6,9 +6,13 @@ from qrao.utils import get_random_maxcut_qp
 
 from qrao.multiqubit import encode, dvar_values_to_state
 
+from docplex.mp.model import Model
+from qiskit_optimization import QuadraticProgram
+from qiskit_optimization.translators import from_docplex_mp
+
 
 def test_11p():
-    problem = get_random_maxcut_qp(degree=3, num_nodes=6, seed=1)
+    problem = get_random_maxcut_qp(degree=1, num_nodes=6, seed=1)
 
     operator, partitions, offset = encode(
         problem, max_qubits_per_partition=1, max_dvars_per_partition=1
@@ -75,14 +79,63 @@ def test_adaptive_11p_21p():
         assert np.isclose(encoding_eval, objective_eval)
 
 
+def test_adaptive_11p_31p():
+    problem = get_random_maxcut_qp(degree=2, num_nodes=7, seed=1)
+
+    operator, partitions, offset = encode(
+        problem, max_qubits_per_partition=1, max_dvars_per_partition=3
+    )
+
+    for dvar_values in product([0, 1], repeat=7):
+        dvar_values = list(dvar_values)
+        state = dvar_values_to_state(dvar_values, partitions)
+        encoding_eval = np.real(state.expectation_value(operator)) + offset
+        objective_eval = (
+            problem.objective.evaluate(dvar_values) * problem.objective.sense.value
+        )
+        assert np.isclose(encoding_eval, objective_eval)
+
+
+# def test_adaptive_21p_31p():
+#     problem = get_random_maxcut_qp(degree=1, num_nodes=16, seed=1)
+
+#     operator, partitions, offset = encode(
+#         problem, max_qubits_per_partition=1, max_dvars_per_partition=3
+#     )
+
+#     for dvar_values in product([0, 1], repeat=16):
+#         dvar_values = list(dvar_values)
+#         state = dvar_values_to_state(dvar_values, partitions)
+#         encoding_eval = np.real(state.expectation_value(operator)) + offset
+#         objective_eval = (
+#             problem.objective.evaluate(dvar_values) * problem.objective.sense.value
+#         )
+#         assert np.isclose(encoding_eval, objective_eval)
+
+
 def test_32p():
-    problem = get_random_maxcut_qp(degree=1, num_nodes=6, seed=1, draw=True)
+    problem = get_random_maxcut_qp(degree=1, num_nodes=6, seed=1)
 
     operator, partitions, offset = encode(
         problem, max_qubits_per_partition=2, max_dvars_per_partition=3
     )
-
     for dvar_values in product([0, 1], repeat=6):
+        dvar_values = list(dvar_values)
+        state = dvar_values_to_state(dvar_values, partitions)
+        encoding_eval = np.real(state.expectation_value(operator)) + offset
+        objective_eval = (
+            problem.objective.evaluate(dvar_values) * problem.objective.sense.value
+        )
+        assert np.isclose(encoding_eval, objective_eval)
+
+
+def test_adaptive_11p_32p():
+    problem = get_random_maxcut_qp(degree=2, num_nodes=7, seed=1)
+
+    operator, partitions, offset = encode(
+        problem, max_qubits_per_partition=2, max_dvars_per_partition=3
+    )
+    for dvar_values in product([0, 1], repeat=7):
         dvar_values = list(dvar_values)
         state = dvar_values_to_state(dvar_values, partitions)
         encoding_eval = np.real(state.expectation_value(operator)) + offset